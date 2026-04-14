f038754e31006d9772b7c8ca216e5ed31e689af6
akalman <dkeka.zkfaks+github@gmail.com>
2026-04-13 18:51:25 -0700

    add in prototype for poi based overworld

---
+++ prototypes/poi-overworld/HardCodedPoiOverworldController.cs
@@ -0,0 +1,75 @@
+using Godot;
+using System.Collections.Generic;
+using Godot.Collections;
+
+public partial class HardCodedPoiOverworldController : Node2D
+{
+	[Export]
+	public Sprite2D BackgroundContainer { get; set; }
+
+	[Export]
+	public Node2D PoiContainer { get; set; }
+
+	private IList<OverworldPoi> POIs { get; set; }
+    private IList<TextureButton> PoiObjects { get; set; }
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		POIs = 
+		[
+			new OverworldPoi {
+				Location = Vector2.Left * 0.2f + Vector2.Up * 0.2f,
+				SpritePath = "sprites/10847340.png"
+			},
+			new OverworldPoi {
+				Location = Vector2.Right * 0.2f + Vector2.Down * 0.2f,
+				SpritePath = "sprites/10935928.png"
+			},
+		];
+		PoiObjects = new Array<TextureButton>();
+
+		var buttonScale = 0.1f;
+		foreach (var poi in POIs)
+		{
+			var texture = GD.Load<Texture2D>($"res://prototypes/poi-overworld/{poi.SpritePath}");
+			var textureSize = texture.GetSize();
+			var button =  new TextureButton()
+			{
+				TextureNormal = texture,
+                TextureHover = texture,
+                TexturePressed = texture,
+				Position = poi.Location * 500,
+				Scale = Vector2.One * buttonScale,
+                AnchorLeft = 0.5f,
+                AnchorRight = 0.5f,
+                AnchorTop = 0.5f,
+                AnchorBottom = 0.5f,
+                OffsetLeft = -textureSize.X * 0.5f * buttonScale + 960 * poi.Location.X,
+                OffsetRight = textureSize.X * 0.5f * buttonScale - 960 * poi.Location.X,
+                OffsetTop = -textureSize.Y * 0.5f * buttonScale + 540 * poi.Location.Y,
+                OffsetBottom = textureSize.Y * 0.5f * buttonScale - 540 * poi.Location.Y,
+			};
+			PoiContainer.AddChild(button);
+			PoiObjects.Add(button);
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		foreach (var button in PoiObjects)
+		{
+			if (button.ButtonPressed)
+			{
+				GD.PushWarning($"Registered click on poi {button.TextureNormal.ResourcePath}");
+			}
+		}
+	}
+
+	private class OverworldPoi
+	{
+		public Vector2 Location { get; set; }
+		public string SpritePath { get; set; }
+	}
+}
---
+++ prototypes/poi-overworld/README.md
@@ -0,0 +1,6 @@
+# POI Overworld
+
+## Resource Credits
+
+- background sprite from https://www.freepik.com/free-vector/vintage-monochrome-old-pirate-treasure-map_7998455.htm
+- poi icon sprites from https://www.freepik.com/serie/210051290
